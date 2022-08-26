a28f30ebe4aa32e986d9a976550761d142bce19c
dem1ess <stanislav_kharchenko@outlook.com>
2022-08-26 19:22:28 +0200

    Merge remote-tracking branch 'origin/master'

--- src/app/api-rates.service.ts
+++ src/app/api-rates.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
@@ -6,11 +6,13 @@ import {HttpClient} from "@angular/common/http";
 })
 export class ApiRatesService {
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   getCurrencyRates(country1: string, country2: string, amount: number) {
     let url = `https://api.exchangerate.host/convert?from=${country1}&to=${country2}&amount=${amount}`
     return this.http.get(url)
   }
 
+//TODO передалеть что бы было только одно обращение к API
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiRatesService} from "./api-rates.service";
 
 
@@ -13,13 +13,14 @@ interface Country {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   to = 1
   from = 1
   countries: Country[] = [
-    {value: 'UAH', viewValue: 'UAH' ,},
+    {value: 'UAH', viewValue: 'UAH',},
     {value: 'USD', viewValue: 'USD'},
     {value: 'EUR', viewValue: 'EUR'},
+    //TODO Добавить больше валют
   ];
   title = 'CurrencyConverter';
   currjson: any = []
@@ -27,10 +28,33 @@ export class AppComponent {
   constructor(private currency: ApiRatesService) {
   }
 
+  ngOnInit(): void {
+    this.getCurrHeader()
+  }
+
+  date!: number
+  euro: number = 1
+  dollar: number = 1
 
   country1: string = this.countries[0].value
-
   country2: string = this.countries[1].value
+
+  getCurrHeader() {
+    this.currency.getCurrencyRates('USD', 'UAH', 1).subscribe(data => {
+      this.currjson = JSON.stringify(data)
+      this.currjson = JSON.parse(this.currjson)
+      console.log(this.currjson)
+      this.dollar = this.currjson.result
+      this.date = this.currjson.date
+      console.log(this.dollar.toFixed(3))
+    })
+    this.currency.getCurrencyRates('EUR', 'UAH', 1).subscribe(basse => {
+      this.currjson = JSON.stringify(basse)
+      this.currjson = JSON.parse(this.currjson)
+      this.euro = this.currjson.result
+      console.log(this.euro.toFixed(3))
+    })
+  }
 
   changeCountry1(a: string) {
     this.country1 = a
@@ -46,15 +70,16 @@ export class AppComponent {
     this.currency.getCurrencyRates(this.country1, this.country2, this.to).subscribe(data => {
       this.currjson = JSON.stringify(data)
       this.currjson = JSON.parse(this.currjson)
-      console.log(this.to)
       this.from = this.to * this.currjson.info.rate
     })
   }
-  onInputTo(){
+
+  onInputTo() {
     this.from = this.to * this.currjson.info.rate
   }
-onInputFrom(){
-  this.to = this.from / this.currjson.info.rate
 
-}
+  onInputFrom() {
+    this.to = this.from / this.currjson.info.rate
+
+  }
 }
